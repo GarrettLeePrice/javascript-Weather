b2f50f3fc4d4882785db6d70b59f8ad84be364c1
Garrett Price and HK Kahng <hk@>
2016-10-11 15:21:35 -0700

    add code to convert to Celsius

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,23 +1,35 @@
 var Weather = require('./../js/weather.js').weatherModule;
+var GetWeather = require('./../js/weather.js').getWeatherModule;
 
 
 
 $(document).ready(function() {
   var currentWeatherObject = new Weather();
+
   var displayHumidity = function(city, humidityData) {
     $('#displayHumidity').text("The humidity in " + city + " is " + humidityData + "%");
-  }
+  };
+
   var displayTemperature = function(city, temperatureData) {
-    $('#displayTemp').text("The temperature in " + city + " is " + currentWeatherObject.getConvertTemp(temperatureData) + "degrees");
-  }
+    $('#displayTemp').text("The temperature in " + city + " is " + temperatureData + " degrees");
+  };
+
   $('#getHumidity').click(function() {
     var city = $('#cityName').val();
     $('#location').val("");
     currentWeatherObject.getHumidity(city, displayHumidity);
   });
+
   $('#getTemp').click(function() {
     var city = $('#cityName').val();
     $('#location').val("");
     currentWeatherObject.getTemperature(city, displayTemperature);
+    var gotWeather = new GetWeather(city);
+  });
+
+  $('#getCelsius').click(function() {
+    var city = $('#cityName').val();
+    $('#location').val("");
+    currentWeatherObject.getTemperatureCelsius(city, displayTemperature);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,35 @@
 var apiKey = require('./../.env').apiKey;
 
+var getConvertTemp = function(kelvin) {
+  return Math.round(kelvin * (9/5) -459.67);
+};
+
+var getConvertKelvinCelsius = function(kelvin) {
+  return Math.round(kelvin -273.15);
+};
+
+function GetWeather(city) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    this.weatherData = response;
+    console.log(this.weatherData);
+    }).fail(function(error) {
+    $('#displayHumidity').text(error.responseJSON.message);
+  });
+}
 
 function Weather() {
-
 }
+
+Weather.prototype.getWeatherData = function (city) {
+  var weatherData = [];
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    weatherData = response;
+    console.log(weatherData);
+    return weatherData;
+    }).fail(function(error) {
+    $('#displayHumidity').text(error.responseJSON.message);
+  });
+};
 
 Weather.prototype.getHumidity = function(city, displayFunction) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
@@ -11,18 +37,25 @@ Weather.prototype.getHumidity = function(city, displayFunction) {
   }).fail(function(error) {
     $('#displayHumidity').text(error.responseJSON.message);
   });
-}
+};
 
 Weather.prototype.getTemperature = function(city, displayFunction) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    displayFunction(city, response.main.temp);
+    var currTemp = getConvertTemp(response.main.temp);
+    displayFunction(city, currTemp);
   }).fail(function(error) {
     $('#displayTemp').text(error.responseJSON.message);
   });
-}
+};
 
-Weather.prototype.getConvertTemp = function(kelvin) {
-  return kelvin*(9/5) -459.67;
-}
+Weather.prototype.getTemperatureCelsius = function(city, displayFunction) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayFunction(city, getConvertKelvinCelsius(response.main.temp));
+  }).fail(function(error) {
+    $('#displayTemp').text(error.responseJSON.message);
+  });
+};
+
 
 exports.weatherModule = Weather;
+exports.getWeatherModule = Weather;
